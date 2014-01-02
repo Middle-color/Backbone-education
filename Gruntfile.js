150a8e811b94533331dc876c1a43c790d4270882
Aleksandr Tytskiy <aleksandr.tytskiy@maxymiser.com>
2014-01-02 19:14:19 +0200

    more complex gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,51 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        path: {
+            app: 'app',
+            js: '<%= path.app %>/js',
+            less: '<%= path.app %>/less',
+            html: '<%= path.app %>/*.html',
+            bowerComponents: 'bower_components'
+        },
+
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>',
+
+        jshint: {
+            options: {
+                curly: true,
+                eqnull: true,
+                eqeqeq: true,
+                undef: true,
+                browser: true,
+                globals: {
+                    console: true,
+                    _: true,
+                    $: true,
+                    jQuery: true,
+                    Backbone: true,
+                    module: true,
+                    require: true,
+                    define: true,
+                    ENV: true
+                }
+            },
+            gruntfile: ['Gruntfile.js'],
+            app: ['<%= path.js %>/**/*.js']
+        },
+
         watch: {
             options: {
                 livereload: true
+            },
+            gruntfile: {
+                files: ['<%= jshint.gruntfile %>'],
+                tasks: ['jshint:gruntfile', 'watch']
+            },
+            app: {
+                files: ['<%= jshint.app %>'],
+                tasks: ['jshint:app']
             },
             html: {
                 files: ['public/index.html'],
@@ -16,6 +58,5 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-
+    grunt.registerTask('default', ['watch']);
 };
