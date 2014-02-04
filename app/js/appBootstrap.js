00057c7da644f7f4969eca01af97951e41de7219
unkowzguy <unkowzguy@dummy>
2014-02-04 15:55:17 +0200

    clean repository to empty state

--- app/js/appBootstrap.js
+++
@@ -1,9 +0,0 @@
-(function (document, window, $, Backbone, undefined) {
-
-    'use strict';
-
-    $(function () {
-        var appView = new app.views.AppView();
-    });
-
-})(document, window, jQuery, Backbone);
